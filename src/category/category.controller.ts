8a8a66556061d6c3c8194e4a16a8ffe641bb5f47
haeseung123 <showui96@gmail.com>
2023-11-17 18:58:32 +0900

    chore: 카테고리 api JwtExceptionFilter 적용
    feat-#23

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, UseFilters, UseGuards } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CategoryResponseMessage } from './classes/category.response.message';
 import { ResponseMessage } from 'src/global/decorators/response-key.decorator';
 import { AtGuard } from 'src/auth/guard/access.token.guard';
+import { JwtExceptionFilter } from 'src/global/filters/jwt-exception.filter';
 
 @Controller('category')
+@UseFilters(JwtExceptionFilter)
 export class CategoryController {
 	constructor(private readonly categoryService: CategoryService) {}
 
